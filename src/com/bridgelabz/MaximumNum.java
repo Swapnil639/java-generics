44a72641e542336462cbaef36bb4bfbfe41083ed
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-20 00:20:17 +0530

    Added :Check Maximum among three Float number

--- src/com/bridgelabz/MaximumNum.java
+++ src/com/bridgelabz/MaximumNum.java
@@ -6,22 +6,23 @@ public class MaximumNum {
     public static void main(String[] args) {
         System.out.println("Welcome to Maximum problem using Generics");
         MaximumNum obj = new MaximumNum();
-        System.out.println("Maximum among three Integer number is :"+obj.compareTo());
+        System.out.println("Maximum among three Float number is :" + obj.compareTo());
 
     }
-    public Integer compareTo(){
-        Scanner scanner= new Scanner(System.in);
-        System.out.print("Enter three Integer Number :");
-        Integer a=scanner.nextInt();
-        Integer b=scanner.nextInt();
-        Integer c=scanner.nextInt();
-        Integer maximum;
 
-        if(a>b && a>c){
-            maximum=a;
-        }else if(b>a && b>c){
-            maximum=b;
-        }else {
+    public Float compareTo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter three Float Number :");
+        Float a = scanner.nextFloat();
+        Float b = scanner.nextFloat();
+        Float c = scanner.nextFloat();
+        Float maximum;
+
+        if (a > b && a > c) {
+            maximum = a;
+        } else if (b > a && b > c) {
+            maximum = b;
+        } else {
             maximum = c;
         }
         return maximum;
