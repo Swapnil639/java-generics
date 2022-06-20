aea8aeba1a151ebad902b22a7aa56116a119fd5b
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-20 07:07:44 +0530

    Added :More than three parameter and find maximum using Sorting

--- src/com/bridgelabz/MaximumNum.java
+++ src/com/bridgelabz/MaximumNum.java
@@ -1,39 +1,18 @@
 package com.bridgelabz;
 
-
-class GenericsClass<T extends Comparable>{
-    T a;
-    T b;
-    T c;
-    public GenericsClass(T a,T b,T c){
-       this.a=a;
-       this.b=b;
-       this.c=c;
-    }
-    public T testcase() {
-        T maximum=a;
-        if (b.compareTo(maximum)>0) {
-            maximum = b;
-        } else if (c.compareTo(maximum)>0) {
-            maximum = c;
-        }
-        return maximum;
-    }
-}
-
-
+import java.util.Arrays;
 
 public class MaximumNum {
+    public static <T> T testcase(T...a){
+        Arrays.sort(a);
+        return a[a.length-1];
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Maximum problem using Generics");
 
-        GenericsClass<Integer> genericsClass = new GenericsClass<>(30,60,20);
-        GenericsClass<Float> genericsClass1 = new GenericsClass<>(1.0f,3.7f,7.6f);
-        GenericsClass<String> genericsClass2 = new GenericsClass<>("Apple","Peach","Banana");
-
-        System.out.println(genericsClass.testcase());
-        System.out.println(genericsClass1.testcase());
-        System.out.println(genericsClass2.testcase());
+        System.out.println("Maximum of Integer number is :"+testcase(10,49,78,45,89,40));
+        System.out.println("Maximum of Float number is :"+testcase(10.2,49.6,78.8,45.0,89.9,40.8));
+        System.out.println("Maximum of String value is :"+testcase("Apple","Banana","Orange","Grapes","Cherry"));
 
 
     }
