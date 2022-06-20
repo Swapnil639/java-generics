85234e13d51b42b44822c4429099c90b33125f13
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-20 06:01:11 +0530

    Added :Refactor all three method to one generic method

--- src/com/bridgelabz/MaximumNum.java
+++ src/com/bridgelabz/MaximumNum.java
@@ -1,34 +1,20 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class MaximumNum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Maximum problem using Generics");
-        MaximumNum obj = new MaximumNum();
-        System.out.println("Maximum among three String is :" + obj.compareTo());
-
-    }
-
-    public String compareTo() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter three String :");
-
-        String a = scanner.nextLine();
-        System.out.println(a);
-
-        String b = scanner.nextLine();
-        System.out.println(b);
-
-        String c = scanner.nextLine();
-        System.out.println(c);
-
-        String maximum=a;
+    public static <T extends Comparable> T testcase(T a,T b,T c) {
+        T maximum=a;
         if (b.compareTo(maximum)>0) {
             maximum = b;
         } else if (c.compareTo(maximum)>0) {
             maximum = c;
         }
         return maximum;
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Maximum problem using Generics");
+        System.out.println("Maximum among three Integer is :"+testcase(30,60,20));
+        System.out.println("Maximum among three Float is :"+testcase(10.0f,30.57f,70.56f));
+        System.out.println("Maximum among three String is :"+testcase("Apple","Peach","Banana"));
+
     }
 }
